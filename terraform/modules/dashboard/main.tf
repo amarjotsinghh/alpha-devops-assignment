9fe0b0ca414c507f2ef696700df6d43b46e77772
amarjotsinghh <amarjotsingh5555@gmail.com>
2026-03-04 20:11:52 +0530

    Fix CloudWatch dashboard metrics for EKS node monitoring

--- terraform/modules/dashboard/main.tf
+++ terraform/modules/dashboard/main.tf
@@ -1,79 +1,119 @@
 resource "aws_cloudwatch_dashboard" "eks_dashboard" {
+
   dashboard_name = "alpha-devops-dashboard"
 
   dashboard_body = jsonencode({
+
     widgets = [
+
       {
-        type = "metric"
-        x    = 0
-        y    = 0
+        type   = "metric"
+        x      = 0
+        y      = 0
         width  = 12
         height = 6
 
         properties = {
           metrics = [
-            [ "AWS/ApplicationELB", "RequestCount", "LoadBalancer", var.alb_name ]
+            [
+              "AWS/ApplicationELB",
+              "RequestCount",
+              "LoadBalancer",
+              "app/k8s-default-alphaing-e4acd881d2/17f299fc1ee6e6f8"
+            ]
           ]
+
           period = 300
           stat   = "Sum"
-          region = var.region
+          region = "ap-southeast-1"
           title  = "ALB Request Count"
         }
       },
 
       {
-        type = "metric"
-        x    = 12
-        y    = 0
+        type   = "metric"
+        x      = 12
+        y      = 0
         width  = 12
         height = 6
 
         properties = {
           metrics = [
-            [ "AWS/ApplicationELB", "TargetResponseTime", "LoadBalancer", var.alb_name ]
+            [
+              "AWS/ApplicationELB",
+              "TargetResponseTime",
+              "LoadBalancer",
+              "app/k8s-default-alphaing-e4acd881d2/17f299fc1ee6e6f8"
+            ]
           ]
+
           period = 300
           stat   = "Average"
-          region = var.region
+          region = "ap-southeast-1"
           title  = "ALB Response Time"
         }
       },
 
       {
-        type = "metric"
-        x    = 0
-        y    = 6
+        type   = "metric"
+        x      = 0
+        y      = 6
         width  = 12
         height = 6
 
         properties = {
           metrics = [
-            [ "AWS/EC2", "CPUUtilization", "AutoScalingGroupName", var.nodegroup_name ]
+            [
+              "AWS/EC2",
+              "CPUUtilization",
+              "InstanceId",
+              "i-0124c0a5b9fa995f6"
+            ],
+            [
+              "AWS/EC2",
+              "CPUUtilization",
+              "InstanceId",
+              "i-044c6daf4f5ef7456"
+            ]
           ]
+
           period = 300
           stat   = "Average"
-          region = var.region
+          region = "ap-southeast-1"
           title  = "EKS Node CPU Utilization"
         }
       },
 
       {
-        type = "metric"
-        x    = 12
-        y    = 6
+        type   = "metric"
+        x      = 12
+        y      = 6
         width  = 12
         height = 6
 
         properties = {
           metrics = [
-            [ "AWS/EC2", "NetworkIn", "AutoScalingGroupName", var.nodegroup_name ]
+            [
+              "AWS/EC2",
+              "NetworkIn",
+              "InstanceId",
+              "i-0124c0a5b9fa995f6"
+            ],
+            [
+              "AWS/EC2",
+              "NetworkIn",
+              "InstanceId",
+              "i-044c6daf4f5ef7456"
+            ]
           ]
+
           period = 300
           stat   = "Sum"
-          region = var.region
+          region = "ap-southeast-1"
           title  = "EKS Node Network In"
         }
       }
+
     ]
   })
 }
