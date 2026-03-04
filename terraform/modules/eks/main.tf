66ae347e67d6ef666d9bbfde5b3f473a96509a17
amarjotsinghh <amarjotsingh5555@gmail.com>
2026-03-04 14:35:03 +0530

    Add EKS managed node group

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -32,3 +32,60 @@ resource "aws_eks_cluster" "this" {
     aws_iam_role_policy_attachment.eks_policy
   ]
 }
+
+
+resource "aws_iam_role" "eks_node_role" {
+  name = "${var.project_name}-${var.environment}-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "node_policy_1" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "node_policy_2" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_role_policy_attachment" "node_policy_3" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+
+
+resource "aws_eks_node_group" "this" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = "${var.project_name}-${var.environment}-nodes"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+
+  subnet_ids = var.private_subnets
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  instance_types = ["t3.medium"]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.node_policy_1,
+    aws_iam_role_policy_attachment.node_policy_2,
+    aws_iam_role_policy_attachment.node_policy_3
+  ]
+}
